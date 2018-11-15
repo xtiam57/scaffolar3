742fa498b5534ce77dd44712fbb19a2e13c1190d
Christiam Mena <christiam.mena@gmail.com>
2018-11-15 17:12:22 -0500

    Restful WIP (adding Observables and handleError)

--- src/app/services/restful.service.ts
+++ src/app/services/restful.service.ts
@@ -1,7 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AppSettings } from '../app.settings';
+import { Observable } from 'rxjs/internal/Observable';
+import { throwError } from 'rxjs/internal/observable/throwError';
+import { catchError, map } from 'rxjs/operators';
 import * as _ from 'underscore';
+import { AppSettings } from '../app.settings';
+import { MessagesService } from './messages.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +13,8 @@ import * as _ from 'underscore';
 export class RESTfulService {
   url: string;
 
-  constructor(private http: HttpClient) {
-    this.url = AppSettings.getInfo().api();
+  constructor(private http: HttpClient, private messageService: MessagesService) {
+    this.url = AppSettings.api;
   }
 
   /**
@@ -30,84 +34,140 @@ export class RESTfulService {
   }
 
   /**
+   * Handle errors
+   * @param error Error information
+   */
+  private handleError(error: HttpErrorResponse | any) {
+    switch (error.status) {
+      case 401:
+        // TODO
+        this.messageService.show('Wrong username/password combination.', null, 'warning');
+        this.messageService.show('Your session has expired. Re-enter your credentials.', null, 'warning');
+        break;
+      case 403:
+        this.messageService.show('Your permissions are not sufficient to access this feature.', null, 'error');
+        break;
+      case 404:
+        this.messageService.show('Sorry but the service you are looking for has not been found.', null, 'error');
+        break;
+      case 400:
+      case 500:
+        this.messageService.show(error.message, null, 'error');
+        break;
+      case -1:
+        this.messageService.show('There is not Internet connection.', null, 'error');
+        break;
+      default:
+        this.messageService.show('No "Access - Control - Allow - Origin" header is present on the requested resource.', null, 'error');
+        break;
+    }
+    return throwError(error.message);
+  }
+
+  /**
+   * Process the response
+   * @param response Element to process
+   */
+  private processResponse(response: any) {
+    return response;
+  }
+
+  /**
+   * Restful call
+   * @param method REST method
+   * @param endpoint Endpoint string
+   * @param payload If any
+   * @param queryStrings Query string object
+   * @param config Settings object
+   */
+  private internalCall(method: string = 'get', endpoint: string, payload: any = null, queryStrings: any, config: any = {}): Observable<{}> {
+    // Config can't be null
+    if (_.isNull(config)) {
+      config = {};
+    }
+    // GET and DELETE methods
+    if (_.isNull(payload) || _.isUndefined(payload)) {
+      return this.http[method](this.createUrl(endpoint, queryStrings), config).pipe(
+        map((response) => this.processResponse(response)),
+        catchError((error) => this.handleError(error))
+      );
+    }
+    // POST, PUT, PATCH methods
+    return this.http[method](this.createUrl(endpoint, queryStrings), payload, config).pipe(
+      map((response) => this.processResponse(response)),
+      catchError((error) => this.handleError(error))
+    );
+  }
+
+  /**
    * GET
    * @return a Promise
    */
-  get(endpoint: string, queryStrings: any, config: any = {}) {
-    const promise = new Promise((resolve, reject) => {
-      this.http
-        .get(this.createUrl(endpoint, queryStrings), config)
-        .toPromise()
-        .then((response) => resolve(response), (msg) => reject(msg));
-    });
-    return promise;
+  get(endpoint: string, queryStrings: any, config: any = {}): Promise<{}> {
+    return this.getObservable(endpoint, queryStrings, config).toPromise();
   }
-
   /**
    * POST
    * @return a Promise
    */
-  post(endpoint: string, payload: any, queryStrings: any, config: any = {}) {
-    if (!payload || !_.isObject(payload)) {
-      payload = {};
-    }
-
-    const promise = new Promise((resolve, reject) => {
-      this.http
-        .post(this.createUrl(endpoint, queryStrings), payload, config)
-        .toPromise()
-        .then((response) => resolve(response), (msg) => reject(msg));
-    });
-    return promise;
+  post(endpoint: string, payload: any, queryStrings: any, config: any = {}): Promise<{}> {
+    return this.postObservable(endpoint, payload, queryStrings, config).toPromise();
   }
-
   /**
    * PUT
    * @return a Promise
    */
-  put(endpoint: string, payload: any, queryStrings: any, config: any = {}) {
-    if (!payload || !_.isObject(payload)) {
-      payload = {};
-    }
-
-    const promise = new Promise((resolve, reject) => {
-      this.http
-        .put(this.createUrl(endpoint, queryStrings), payload, config)
-        .toPromise()
-        .then((response) => resolve(response), (msg) => reject(msg));
-    });
-    return promise;
+  put(endpoint: string, payload: any, queryStrings: any, config: any = {}): Promise<{}> {
+    return this.putObservable(endpoint, payload, queryStrings, config).toPromise();
   }
-
   /**
    * PATCH
    * @return a Promise
    */
-  patch(endpoint: string, payload: any, queryStrings: any, config: any = {}) {
-    if (!payload || !_.isObject(payload)) {
-      payload = {};
-    }
-
-    const promise = new Promise((resolve, reject) => {
-      this.http
-        .patch(this.createUrl(endpoint, queryStrings), payload, config)
-        .toPromise()
-        .then((response) => resolve(response), (msg) => reject(msg));
-    });
-    return promise;
+  patch(endpoint: string, payload: any, queryStrings: any, config: any = {}): Promise<{}> {
+    return this.patchObservable(endpoint, payload, queryStrings, config).toPromise();
   }
-
   /**
    * DELETE
    * @return a Promise
    */
-  delete(endpoint: string, queryStrings: any, config: any = {}) {
-    const promise = new Promise((resolve, reject) => {
-      this.http
-        .delete(this.createUrl(endpoint, queryStrings), config)
-        .toPromise()
-        .then((response) => resolve(response), (msg) => reject(msg));
-    });
-    return promise;
+  delete(endpoint: string, queryStrings: any, config: any = {}): Promise<{}> {
+    return this.deleteObservable(endpoint, queryStrings, config).toPromise();
+  }
+
+  /**
+   * GET Observable
+   * @return a Observable
+   */
+  getObservable(endpoint: string, queryStrings: any, config: any = {}): Observable<{}> {
+    return this.internalCall('get', endpoint, null, queryStrings, config);
+  }
+  /**
+   * POST Observable
+   * @return a Observable
+   */
+  postObservable(endpoint: string, payload: any, queryStrings: any, config: any = {}): Observable<{}> {
+    return this.internalCall('post', endpoint, payload, queryStrings, config);
+  }
+  /**
+   * PUT Observable
+   * @return a Observable
+   */
+  putObservable(endpoint: string, payload: any, queryStrings: any, config: any = {}): Observable<{}> {
+    return this.internalCall('put', endpoint, payload, queryStrings, config);
+  }
+  /**
+   * PATCH Observable
+   * @return a Observable
+   */
+  patchObservable(endpoint: string, payload: any, queryStrings: any, config: any = {}): Observable<{}> {
+    return this.internalCall('patch', endpoint, payload, queryStrings, config);
+  }
+  /**
+   * DELETE Observable
+   * @return a Observable
+   */
+  deleteObservable(endpoint: string, queryStrings: any, config: any = {}): Observable<{}> {
+    return this.internalCall('delete', endpoint, null, queryStrings, config);
   }
 }
